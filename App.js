1541d1fb90e1467915493a5499b944be65fcbd5c
ashrx19 <ashwin191205@gmail.com>
2026-07-09 17:05:23 +0530

    EXPRESS APP

--- App.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express()
-
-app.listen(9999);
-
-app.get('/',(req,res)=>{
-    res.send("Hello Express!")
-})
---
+++ EXPRESS APP/App.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+app.listen(9999);
+
+app.get('/',(req,res)=>{
+     res.sendFile('./home.html', {root: __dirname + '/../components'});
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile('./about.html',{root: __dirname + '/../components'})
+})
+
+//REDIRECTING
+app.get('/joinus', (req, res)=>{
+    res.redirect('/home');})
+
+//Error
+app.use((req, res)=>{
+    res.sendFile('./error.html',{root: __dirname + '/../components'})
+})
