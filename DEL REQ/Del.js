8e8273dd662cc677f23177ac2a218d15e8bb7edf
ashrx19 <ashwin191205@gmail.com>
2026-07-21 17:23:47 +0530

    DEL REQ

---
+++ DEL REQ/Del.js
@@ -0,0 +1,40 @@
+import express from "express"
+
+const app = express()
+const PORT = 9999;
+
+app.use(express.json());
+
+const user = [
+    {id:1, name:"Ashwin", age:18},
+    {id:2, name:"Gokul", age:19},
+    {id:3, name:"Kison", age:18},
+
+]
+
+const products = [
+  { id: 1, name: "iPhone", category: "mobile" },
+  { id: 2, name: "MacBook", category: "laptop" },
+  { id: 3, name: "Samsung", category: "mobile" }
+];
+
+app.delete("/Products/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const productIndex = products.findIndex((productIndex)=>productIndex.id===id);
+
+    if(productIndex===-1){
+        return res.status(200).json({Message:"Product index not found"})
+    }
+
+    products.splice(productIndex, 1);
+
+    res.status(200).json({
+    message: "productIndex updated successfully",
+    products: products,
+    });
+
+})
+
+app.listen(PORT,()=>{
+    console.log("App is running")
+})
